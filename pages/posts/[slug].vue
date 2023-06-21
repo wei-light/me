9bd632f3afb5ec330c93e53c87036fe2b0f84e36
tinylight <wei_light@qq.com>
2023-06-21 16:12:15 +0800

    update

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -1,9 +1,58 @@
 <script setup lang="ts">
+const router = useRouter()
 
+const content = ref<HTMLDivElement>()
+
+onMounted(() => {
+  const navigate = () => {
+    if (location.hash) {
+      document.querySelector(decodeURIComponent(location.hash))
+        ?.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
+  const handleAnchors = (
+    event: MouseEvent & { target: HTMLElement },
+  ) => {
+    const link = event.target.closest('a')
+
+    if (
+      !event.defaultPrevented
+      && link
+      && event.button === 0
+      && link.target !== '_blank'
+      && link.rel !== 'external'
+      && !link.download
+      && !event.metaKey
+      && !event.ctrlKey
+      && !event.shiftKey
+      && !event.altKey
+    ) {
+      const url = new URL(link.href)
+      if (url.origin !== window.location.origin) return
+
+      event.preventDefault()
+
+      const { pathname, hash } = url
+      if (hash && (!pathname || pathname === location.pathname)) {
+        window.history.replaceState({}, '', hash)
+        navigate()
+      }
+      else {
+        router.push({ path: pathname, hash })
+      }
+    }
+  }
+
+  useEventListener(window, 'hashchange', navigate)
+  useEventListener(content.value!, 'click', handleAnchors, { passive: false })
+
+  setTimeout(navigate, 500)
+})
 </script>
 
 <template>
-  <div>
+  <div ref="content">
     <ContentDoc v-slot="{ doc }">
       <div class="mb-8">
         <h1 class="text-3xl font-bold text-gray-800 mb-6">
