e637e0ecd71bd5e4f2e1d8fc0771dd8658597461
tinylight <wei_light@qq.com>
2023-06-21 09:19:37 +0800

    home

---
+++ components/PostItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string
+  date?: Date | string
+  href?: string
+}>()
+</script>
+
+<template>
+  <NuxtLink :to="href" class="flex justify-between -mx-3 px-3 py-2 rounded transition-colors hover:bg-gray-100">
+    <h2 class="line-clamp-1 mr-8">
+      {{ title }}
+    </h2>
+    <div class="flex-shrink-0">
+      {{ useDateFormat(date, 'YYYY-MM-DD').value }}
+    </div>
+  </NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,51 @@
 </script>
 
 <template>
-  <div>
-    Home
+  <div class="flex flex-col gap-12">
+    <div>
+      <BlockTitle title="关于我" background-text="About" />
+      <article class="prose">
+        <ContentDoc path="/about" />
+      </article>
+    </div>
+    <div>
+      <BlockTitle title="博客" background-text="Blogs" />
+      <PostList type="blog" class="mt-5">
+        <template #default="{ list }">
+          <PostItem
+            v-for="article of list"
+            :key="article._path"
+            :title="article.title"
+            :date="article.date"
+            :href="article._path"
+          />
+        </template>
+        <template #not-found>
+          <p class="text-gray-500 py-4">
+            No articles found.
+          </p>
+        </template>
+      </PostList>
+    </div>
+    <div>
+      <BlockTitle title="周记" background-text="Weekly" />
+      <PostList type="weekly" class="mt-5">
+        <template #default="{ list }">
+          <PostItem
+            v-for="article of list"
+            :key="article._path"
+            :title="article.title"
+            :date="article.date"
+            :href="article._path"
+          />
+        </template>
+        <template #not-found>
+          <p class="text-gray-500 py-4">
+            No articles found.
+          </p>
+        </template>
+      </PostList>
+    </div>
   </div>
 </template>
 
