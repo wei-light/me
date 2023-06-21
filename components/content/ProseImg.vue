2474699070cd59e761a139fdbb4fb348beebb976
tinylight <wei_light@qq.com>
2023-06-21 09:35:19 +0800

    enhance prose img

---
+++ components/content/ProseImg.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { withBase } from 'ufo'
+import { computed, useRuntimeConfig } from '#imports'
+
+const props = defineProps({
+  src: {
+    type: String,
+    default: '',
+  },
+  alt: {
+    type: String,
+    default: '',
+  },
+  width: {
+    type: [String, Number],
+    default: undefined,
+  },
+  height: {
+    type: [String, Number],
+    default: undefined,
+  },
+})
+const refinedSrc = computed(() => {
+  if (props.src?.startsWith('/') && !props.src.startsWith('//')) {
+    return withBase(props.src, useRuntimeConfig().app.baseURL)
+  }
+  return props.src
+})
+</script>
+
+<template>
+  <figure>
+    <ImageZoom
+      :src="refinedSrc"
+      :alt="alt"
+      :width="width"
+      :height="height"
+      :options="{ background: 'rgba(0, 0, 0, 0.7)' }"
+    />
+    <figcaption v-if="alt">
+      {{ alt }}
+    </figcaption>
+  </figure>
+</template>
