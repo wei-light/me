5dc8ec8bd80d3672c3492c49326454891f8bf392
tinylight <wei_light@qq.com>
2023-06-21 09:21:27 +0800

    content page

---
+++ pages/posts/[slug].vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <ContentDoc v-slot="{ doc }">
+      <div class="mb-8">
+        <h1 class="text-3xl font-bold text-gray-800 mb-6">
+          {{ doc.title }}
+        </h1>
+        <div class="text-gray-500">
+          {{ useDateFormat(doc.date, 'MMM DD, YYYY', { locales: 'en-US' }).value }}
+        </div>
+      </div>
+      <ContentRenderer :value="doc" tag="article" class="prose" />
+    </ContentDoc>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
