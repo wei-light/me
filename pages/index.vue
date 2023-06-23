4a52255daf0552c3792b26589146e4e2749aa6f4
tinylight <wei_light@qq.com>
2023-06-23 12:23:47 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
-
+useSeoMeta({
+  title: 'About me - Bright',
+  ogTitle: 'About me - Bright',
+  description: 'Bright\'s Profile',
+  ogDescription: 'Bright\'s Profile',
+})
 </script>
 
 <template>
@@ -7,7 +12,7 @@
     <div>
       <BlockTitle title="关于我" background-text="About" />
       <article class="prose">
-        <ContentDoc path="/about" />
+        <ContentDoc path="/about" :head="false" />
       </article>
     </div>
     <div>
