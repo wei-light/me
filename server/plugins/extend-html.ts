33c00254f9fba8e90883f35027c0b5433aaecf92
tinylight <wei_light@qq.com>
2023-07-03 09:14:03 +0800

    fix page twinkle

--- server/plugins/extend-html.ts
+++ server/plugins/extend-html.ts
@@ -1,6 +1,16 @@
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('render:html', (html) => {
     // This will be an object representation of the html template.
+    html.head.unshift(`
+      <script>
+        (function () {
+          const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+          const setting = localStorage.getItem('vueuse-color-scheme') || 'auto'
+          if (setting === 'dark' || (prefersDark && setting !== 'light'))
+            document.documentElement.classList.toggle('dark', true)
+        })()
+      </script>
+    `)
     html.body.push('<div id="portal"></div>')
   })
 })
