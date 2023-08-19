64236ef32de88866bcb20cf70a2d997e949900e0
tinylight <wei_light@qq.com>
2023-08-19 11:32:14 +0800

    style

--- components/DocList.vue
+++ components/DocList.vue
@@ -12,7 +12,7 @@ defineProps<{
       v-for="doc of docs"
       :key="doc.name"
       :to="doc.href"
-      class="relative overflow-hidden h-40 p-4 bg-gray-100/70 border border-gray-200/50 rounded-md transition-transform group hover:-translate-y-1 dark:bg-neutral-800/70 dark:border-neutral-700/50"
+      class="relative overflow-hidden h-40 p-4 bg-gray-100/70 rounded-md transition-transform group hover:-translate-y-1 dark:bg-neutral-800/70 dark:border-neutral-700/50"
     >
       <p class="font-bold text-sm opacity-95">
         {{ doc.name }}
